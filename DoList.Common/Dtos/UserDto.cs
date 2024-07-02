175f58631c2d3e47e973483a8ae64d563f09cef8
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-07-02 20:01:05 +0500

    UserDto

---
+++ DoList.Common/Dtos/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoList.Common.Dtos
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public List<TasksDto> Tasks { get; set; }
+    }
+}
