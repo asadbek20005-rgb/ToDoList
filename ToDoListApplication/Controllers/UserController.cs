483f441133685ef5f2781141ca7565aa2808247a
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-07-02 15:17:47 +0500

    User&&TaskControllers

---
+++ ToDoListApplication/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using DoList.Service.ModelServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController(UserService userService) => _userService = userService;
+        
+        private readonly UserService _userService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
