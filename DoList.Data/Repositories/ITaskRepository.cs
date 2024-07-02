91963d1d8a6a4eec01137f5c31ef44173c7275ae
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-07-02 12:50:45 +0500

    ITaskRepos

---
+++ DoList.Data/Repositories/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using DoList.Data.Entities;
+
+namespace DoList.Data.Repositories
+{
+    public interface ITaskRepository
+    {
+        public Task<List<Tasks>> GetAllTasks();
+        public Task<Tasks> GetTaskById(Guid userId);
+        public Task<Tasks> GetUserTaskByName(string userName);
+        public Task AddTask(Tasks user);
+        public Task UpdateTask(Tasks user);
+        public Task DeleteTask(Tasks userId);
+    }
+}
