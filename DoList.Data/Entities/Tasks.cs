3171a2d8df39fb5e3a34986a5ad1b2f1b8804f2b
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-07-03 11:48:48 +0500

    AddTaskModel

--- DoList.Data/Entities/Tasks.cs
+++ DoList.Data/Entities/Tasks.cs
@@ -6,6 +6,8 @@
         public string Taskname {  get; set; }
         public string Description { get; set; }
         public string TaskType { get; set; }
+        public DateOnly DueDate { get; set; }
+        public TimeOnly DueTime { get; set; }
         public bool IsCompleted { get; set; }   
 
         public Guid UserId { get; set; }
