89f1f56c13c61abc1ec98a36dc35eeb10769d8b1
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-07-04 12:23:21 +0500

    GetTask Method

--- DoList.Service/ModelServices/TaskService.cs
+++ DoList.Service/ModelServices/TaskService.cs
@@ -1,13 +1,41 @@
-﻿using DoList.Data.Context;
+﻿using DoList.Common.Dtos;
+using DoList.Common.Models.Task;
+using DoList.Data.Entities;
+using DoList.Data.Repositories;
+using DoList.Service.ConvertToExtension;
 
 namespace DoList.Service.ModelServices
 {
     public class TaskService
     {
-        public TaskService(AppDbContext appDbContext)
+        public TaskService(ITaskRepository taskRepository)
         {
-            _dbContext = appDbContext;
+            _taskRepository = taskRepository;
         }
-        private readonly AppDbContext _dbContext;
+        private readonly ITaskRepository _taskRepository;
+
+        public async Task<TasksDto> AddTask(AddTaskModel model)
+        {
+            var newTask = new Tasks
+            {
+                Taskname = model.Taskname,
+                Description = model.Description,
+                TaskType = model.TaskType,
+                DueDate = model.DueDate,
+                DueTime = model.DueTime,
+            };
+
+            await _taskRepository.AddTask(newTask);
+
+            return newTask.ParseToModel();
+        }
+
+
+        public async Task<TasksDto> GetTask(int taskId)
+        {
+            var task = await _taskRepository.GetTaskById(taskId) ??
+                 throw new Exception($"Task with id {taskId} not found.");
+            return task.ParseToModel();
+        }
     }
 }
